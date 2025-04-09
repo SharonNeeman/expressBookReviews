4297914b8ff3050e67d34e4a2c51cb7163d2070c
SharonNeeman <srn.nmn@outlook.com>
2025-04-09 16:05:55 -0400

    task11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -56,8 +56,27 @@ public_users.get('/', function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
+    const bookPromise = new Promise((resolve, reject) => {
+        const book = books[isbn]
+        if (book) {
+            // If the book exists, resolve the promise with the book
+            resolve(book);
+          } else {
+            // If the book doesn't exist, reject the promise with an error
+            reject("Book not found");
+          }
+    });
+    bookPromise.then((book)=>res.send(book))
+                .catch( (error)=> res.status(404).send(error))
+ });
+
+ /*
+ // Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
     res.send(books[isbn]);
  });
+ */
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
